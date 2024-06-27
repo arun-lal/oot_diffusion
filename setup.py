e4f2842a0afb397b6c55a9db52775397d3edf30d
Arun Lal <96575610+arun-lal@users.noreply.github.com>
2024-06-27 20:26:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author="Viktor Frede Andersen",
     author_email="vikfand@gmail.com",
     url="https://github.com/arun-lal/ootd_diffusion.git",
-    packages=find_packages(include=["ootd_diffusion", "ootd_diffusion.*"]),
+    packages=find_packages(include=["oot_diffusion", "oot_diffusion.*"]),
     include_package_data=True,
     install_requires=[
         "torch",
